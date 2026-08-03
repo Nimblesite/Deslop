525345b7932836e1f40353e73b22292599cc7eef
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2026-08-04 06:45:29 +1000

    Fixes

---
+++ site/playwright.config.js
@@ -0,0 +1,9 @@
+export default {
+  testDir: "./tests",
+  use: { baseURL: "http://127.0.0.1:8092" },
+  webServer: {
+    command: "npm run dev -- --port=8092",
+    url: "http://127.0.0.1:8092/",
+    reuseExistingServer: false,
+  },
+};
